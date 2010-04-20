98ab38a20bd47fea8c856e395fe213a3d9ebd028
root <root@snag.easynews.com>
2010-04-20 14:55:11 -0700

    syntax error

--- lib/SNAG/Source/SystemInfo/Linux.pm
+++ lib/SNAG/Source/SystemInfo/Linux.pm
@@ -130,7 +130,7 @@ sub build_config_file_list
 
   # add all config files for vservers
   my @vservers = glob('/etc/vservers/*');
-  $default_config_dirs{$_ . '/'} = '.' for @list;
+  $default_config_dirs{$_ . '/'} = '.' for @vservers;
   
   foreach my $dir (keys %default_config_dirs)
   {
