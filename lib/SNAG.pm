daed5c7242a476f3a5db2ceeba990c3280e986cb
Rick Strong <rjstrong@gmail.com>
2010-12-30 14:31:16 -0700

    Added new setters for hostname and uuid, and now export UUID.  Also fixed CLIENT_CONF sub return value

--- lib/SNAG.pm
+++ lib/SNAG.pm
@@ -2,7 +2,7 @@ package SNAG;
 
 our @ISA = qw(Exporter);
 
-our @EXPORT = qw( VERSION PARCEL_SEP REC_SEP LOG_DIR LINE_SEP RRD_SEP INFO_SEP SCRIPT_NAME CHECK_HOST_NAME HOST_NAME BASE_DIR CFG_DIR STATE_DIR MOD_DIR OS OSDIST OSVER OSLONG SITE_PERL logger daemonize already_running TMP_DIR SMTP SENDTO CONF);
+our @EXPORT = qw( VERSION PARCEL_SEP REC_SEP LOG_DIR LINE_SEP RRD_SEP INFO_SEP SCRIPT_NAME CHECK_HOST_NAME HOST_NAME BASE_DIR CFG_DIR STATE_DIR MOD_DIR OS OSDIST OSVER OSLONG SITE_PERL logger daemonize already_running TMP_DIR SMTP SENDTO CONF CLIENT_CONF SET_HOST_NAME SET_UUID UUID);
 
 use Exporter;
 
@@ -253,10 +253,10 @@ sub CHECK_HOST_NAME
 }
 
 my $host_name = CHECK_HOST_NAME;
-
+my $uuid = 0;
 
 sub HOST_NAME { $host_name };
-sub CLIENT_CONF { return LOG_DIR . '/client.conf'; };
+sub CLIENT_CONF { return $conf->{directory}->{log_dir} . '/client.conf'; };
 
 sub SET_HOST_NAME
 {
@@ -264,6 +264,13 @@ sub SET_HOST_NAME
   $host_name = $new;
 }
 
+sub SET_UUID
+{
+  my $u = shift || 0;
+  $uuid = $u; 
+}
+
+sub UUID { return $uuid };
 my $name = basename $0;
 sub SCRIPT_NAME { $name };
 
