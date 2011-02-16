3d4aa95e5513d071a42b473c310901e82b37dc77
Zeph Gillen <zeph.gillen@gmail.com>
2011-02-16 23:16:48 +0000

    default to gentoo for os in wrappers

--- aux/makesnag.sh
+++ aux/makesnag.sh
@@ -21,7 +21,12 @@ then
   echo "No makeself found"
 fi
 $makeself --copy snag_installer snag_installer.sh "SNAG binary installer" ./install.sh
-package=`uname -a | awk '{if ($0 ~ /gentoo/) {printf "gentoo"} else if ($0 ~ /Ubuntu/) {printf "ubuntu"} if ($0 ~ /x86_64/) {printf "-x86_64"} else {printf "-x86"}}'`
+
+os=`uname -a | awk 'BEGIN {IGNORECASE=1} {if ($0 ~ /Gentoo/) {printf "gentoo"} else if ($0 ~ /Ubuntu/) {printf "ubuntu"}}'`
+arch=`uname -a | awk '{if ($0 ~ /x86_64/) {printf "x86_64"} else {printf "x86"}}'`
+: ${os:="gentoo"}
+package=$os-$arch
+
 mv snag_installer.sh snag-installer-${package}.sh
 
 cd -
