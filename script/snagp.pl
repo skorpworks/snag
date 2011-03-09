e25101d2675f079af60c02ffb112fdda053b9509
kevinspencer <kevin@kevinspencer.org>
2011-03-09 13:14:55 -0700

    can now compile to a binary

--- script/snagp.pl
+++ script/snagp.pl
@@ -23,7 +23,76 @@ foreach my $arg (@ARGV)
 
 ### Get rid of this once all sources are converted to dispatching
 my %options;
-GetOptions(\%options, 'debug', 'verbose');
+GetOptions(\%options, 'debug', 'verbose', 'compile');
+
+if($options{compile})
+{
+  unless($ENV{PAR_SPAWNED})
+  {
+    my $dest_bin = 'snagp';
+    my $src_script = catfile( $Bin, $Script );
+
+    print "Compiling $src_script to $dest_bin ... ";
+    my $cmd = '
+               /opt/snag/bin/pp --compile "/opt/snag/bin/snagp.pl" 
+               --bundle
+               -M XML::SAX::PurePerl  
+               -M Crypt::Blowfish  
+               -M POE::Filter::Reference  
+               -M POE::Wheel::Run  
+               -M Date::Parse 
+               -M DBM::Deep 
+               -M Data::Dumper
+               -M DBM::Deep::Engine::File 
+               -M DBM::Deep::Iterator::File 
+               -M Crypt::Blowfish  
+               -M Net::Ping 
+               -M Sys::Syslog
+               -M SNAG  
+               -M SNAG::Source::apache
+               -M SNAG::Source::xen 
+               -M SNAG::Source::mysql
+               -M SNAG::Source::vserver
+               -M SNAG::Source::apache_logs
+               -M SNAG::Source::monitor     
+               -M SNAG::Source::stormcellar     
+               -M SNAG::Source::SystemInfo 
+               -M SNAG::Source::SystemInfo::Linux 
+               -M SNAG::Source::SystemStats 
+               -M SNAG::Source::SystemStats::Linux 
+               -a "/opt/snag/snag.conf" 
+               -a "/opt/snag/lib/perl5/site_perl/5.12.1/XML/SAX/ParserDetails.ini;ParserDetails.ini" 
+               --lib="/root/perl5/lib"  
+               --reusable 
+               -o snagp
+              ';
+
+    $cmd =~ s/([\n\r\l])+/ /g;
+
+    print "with cmd $cmd\n";
+    my $out;
+    open LOG, "$cmd |" || die "DIED: $!\n";
+    while (<LOG>)
+    {
+      print $_;
+      $out .= $_;
+    }
+
+    print "Done!\n";
+
+    if($out =~ /\w/)
+    {
+      print "=================== DEBUG ==================\n";
+      print $out;
+    }
+  }
+  else
+  {
+    print "This is already a compile binary!\n";
+  }
+
+  exit;
+}
 
 my $debug = $SNAG::flags{debug};
 
