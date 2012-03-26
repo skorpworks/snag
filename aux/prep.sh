168bc2985509840362be207a0a9ef16511fa8be8
Zeph Gillen <zeph.gillen@gmail.com>
2012-03-26 17:13:48 +0000

    just added output

--- aux/prep.sh
+++ aux/prep.sh
@@ -7,6 +7,10 @@ export PP_INCLUDES=/var/tmp/snag/script/includes/pp_includes
 export SNAGC_INCLUDES=/var/tmp/snag/script/includes/snagc_includes
 export SNAGX_INCLUDES=/var/tmp/snag-x/script/includes/snagx_includes
 
+echo "PP_INCLUDES=$PP_INCLUDES"
+echo "SNAGC_INCLUDES=$SNAGC_INCLUDES"
+echo "SNAGX_INCLUDES=$SNAGX_INCLUDES"
+
 cd /var/tmp/snag \
 && perl Makefile.PL && make && make test && make install \
 && cd /var/tmp \
