fbf7fb2894d6976a4ad34cc778674ac84418778e
Jason Lavold <jlavold@gmail.com>
2017-02-16 13:41:35 -0700

    testing

--- lib/SNAG.pm
+++ lib/SNAG.pm
@@ -27,7 +27,7 @@ GetOptionsFromArray( \@argv_cp, \%flags, 'debug+', 'verbose+', 'init', 'compile'
                      'source=s', 'module=s', 'nowait', 'startatend', 'fullset', 'syslog!',
           );
 
-our $VERSION = '5.16';
+our $VERSION = '5.17';
 sub VERSION { $VERSION };
 
 my ($os, $dist, $ver, $long, $name);
