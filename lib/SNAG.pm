491c06a3133cbc6c968556fc6d205e534854f93b
Jason Lavold <jlavold@gmail.com>
2017-04-18 13:35:15 -0700

    upping version

--- lib/SNAG.pm
+++ lib/SNAG.pm
@@ -27,7 +27,7 @@ GetOptionsFromArray( \@argv_cp, \%flags, 'debug+', 'verbose+', 'init', 'compile'
                      'source=s', 'module=s', 'nowait', 'startatend', 'fullset', 'syslog!',
           );
 
-our $VERSION = '5.17';
+our $VERSION = '5.18';
 sub VERSION { $VERSION };
 
 my ($os, $dist, $ver, $long, $name);
